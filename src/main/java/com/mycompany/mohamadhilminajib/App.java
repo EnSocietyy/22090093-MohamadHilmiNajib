482a2e43b9eea0cbc1ba7f167286e98c30b76a94
EnSocietyy <142369638+EnSocietyy@users.noreply.github.com>
2023-10-07 15:58:49 +0700

    Update App.java

--- src/main/java/com/mycompany/mohamadhilminajib/App.java
+++ src/main/java/com/mycompany/mohamadhilminajib/App.java
@@ -8,6 +8,27 @@ package com.mycompany.mohamadhilminajib;
  *
  * @author MyBook14H
  */
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class App {
 
     public static void main(String[] args) {
